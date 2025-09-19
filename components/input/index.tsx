43ba5cc77619feb762af4eb24061152b43c7c897
Tyler <ty.ho1126@gmail.com>
2025-09-19 09:39:15 +0800

    feat: add form validation and file upload handling to input component

--- components/input/index.tsx
+++ components/input/index.tsx
@@ -1,11 +1,59 @@
 "use client";
 
+import { useRef } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 import { AiOutlinePaperClip, AiOutlinePlus } from "react-icons/ai";
+import { z } from "zod";
+
+const fileSchema = z
+  .instanceof(FileList, { message: "請選擇一個文件" })
+  // .refine((files) => files.length > 1, {
+  //   message: "請選擇一個文件",
+  // })
+  .refine(
+    (files) => {
+      if (!files[0]) return false;
+      return files[0].size <= 50 * 1024 * 1024;
+    },
+    {
+      message: "文件大小不能超過50MB",
+    }
+  );
+
+const schema = z.object({
+  text: z.string().min(1, "請輸入分析目標或方向"),
+  // source: z.string().optional(),
+  file: fileSchema.optional(),
+});
 
 /**
  * 三合一的輸入框，可以添加文字、來源網址、附檔
  */
 export default function InputField() {
+  const sourceRef = useRef<HTMLInputElement>(null);
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: zodResolver(schema) });
+
+  console.log(errors);
+  const { ref: fileRef, ...rest } = register("file");
+
+  function handleAddSource() {
+    sourceRef.current?.click();
+  }
+  function handleAddFile() {
+    console.log("handleAddFile");
+    fileInputRef.current?.click();
+  }
+  function onSubmit(data: unknown) {
+    console.log(data);
+  }
+
   return (
     <div className="space-y-2 w-2/5">
       <p></p>
@@ -14,40 +62,61 @@ export default function InputField() {
         className="p-2 px-3 rounded-lg min-w-xs max-w-xl"
         style={{ boxShadow: "rgba(0, 0, 0, 0.16) 0px 1px 4px" }}
       >
-        <input
-          type="text"
-          className="w-full min-h-8 text-wrap overflow-hidden resize-none outline-none"
-          id="textInput"
-          autoComplete="off"
-          placeholder="請輸入分析目標或方向"
-        />
-
-        <hr className="my-1 border-neutral-300" />
-
-        <section className="flex space-x-1 text-neutral-500">
-          <button className="flex items-center space-x-1">
-            <AiOutlinePlus className="size-5" />
-            <p>新增來源</p>
-          </button>
-          <button className="p-0 flex items-center space-x-1">
-            <AiOutlinePaperClip className="size-5" />
-            <p>添加檔案</p>
-          </button>
-
-          {/*  */}
+        <form onSubmit={handleSubmit(onSubmit)}>
           <input
-            type="url"
-            className="hidden"
-            id="urlInput"
-            placeholder="輸入網址"
-          />
-          <input
-            type="file"
-            className="hidden"
-            accept=".pdf,.doc,.docx,.txt"
-            id="fileInput"
-          />
-        </section>
+            {...register("text", { required: true })}
+            className="w-full overflow-hidden resize-none outline-none min-h-10"
+            id="textInput"
+            autoComplete="off"
+            placeholder="請輸入分析目標或方向"
+          ></input>
+
+          <hr className="my-1 border-neutral-300" />
+
+          <section className="flex justify-between items-center text-neutral-500 w-full">
+            <div className="w-fit space-x-3">
+              {/* <button className="flex items-center space-x-1" onClick={handleAddSource}>
+              <AiOutlinePlus className="size-5" />
+              <p>新增來源</p>
+            </button> */}
+              <button
+                type="button"
+                className="p-0 flex items-center space-x-1"
+                onClick={handleAddFile}
+              >
+                <AiOutlinePaperClip className="size-5" />
+                <p>添加檔案</p>
+              </button>
+            </div>
+
+            <button
+              type="submit"
+              className="font-semibold rounded-lg bg-amber-200 px-3 py-1.5 active:bg-amber-300 active:scale-95 transition-all text-neutral-800"
+            >
+              開始分析
+            </button>
+
+            {/* input element */}
+            {/* <input
+              ref={sourceRef}
+              type="url"
+              className="hidden"
+              id="urlInput"
+              placeholder="輸入網址"
+            /> */}
+            <input
+              {...rest}
+              ref={(e) => {
+                fileInputRef.current = e;
+                fileRef(e);
+              }}
+              type="file"
+              className="hidden"
+              // accept=".pdf,.doc,.docx,.txt"
+              id="file"
+            />
+          </section>
+        </form>
       </div>
     </div>
   );
